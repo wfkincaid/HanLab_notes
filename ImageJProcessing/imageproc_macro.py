a2e01478861555795bed6b94b6dbf74a270f1d05
Warren Kincaid <wfkincai@syr.edu>
2026-05-02 14:57:41 -0500

    modified for image seletion instances, not tested yet

--- ImageJProcessing/imageproc_macro.py
+++ ImageJProcessing/imageproc_macro.py
@@ -2,9 +2,9 @@ import re
 import subprocess
 import os
 from pathlib import Path
-import imagej
+import pandas as pd
 
-imagej_path = "C:/Users/WarrenKincaid/Downloads/ij154-win-java8/ImageJ/ImageJ.exe"
+# imagej_path = "C:/Users/WarrenKincaid/Downloads/ij154-win-java8/ImageJ/ImageJ.exe"
 proc_directory = "C:/Users/WarrenKincaid/git/notebook_wk_hanlab/ImageJProcessing/"
 
 def modify_macro(
@@ -12,22 +12,36 @@ def modify_macro(
     time_stamp,
     trial_number,
     save_configs = None,
-    macro_file = "Modsplat_autothreshold_updated.ijm",
+    selectIm_configs = None,
+    macro_file = "Modsplat_autothreshold_updated_um.ijm",
 ):
     """
     Primer
     
+    This function modifies a text-based .ijm macro file to be run in imageJ
+    
+    It opens a defined image file in the local saved Candice's 
+
+    ----
+
     Args -
         filenumber: digit(s); Candice filename file number, #/## in "_T000#" or "_T00##"
-        time_stamp: digit(s); time in minutes
-        save_configs: List of dicts for each saveAs instance, eg:
+        time_stamp: digit(s); time in minutes of image corresponding with the file's image number ('filenumber')
+        trial_number: digit(s); trial number in file name used as other folder for all files
+        save_configs: None; this becomes set to a dictionary for each saveAs instance, eg:
             [
                 {'suffix': '_edit1'},           # 1st saveAs
                 {'suffix': '_edit2'},           # 2nd saveAs
                 {'suffix': '_edit3'},           # 3rd saveAs
             ]
-        macro_file = original macro name to be opened, edited and saved under modified name
-        
+        selectim_configs = None; this becomes set to a dictionary
+        macro_file = original macro name to be opened, edited and saved under
+        modified name, currently set to the base macro .ijm file for the
+        initial automated processing of an image to reach an image which is
+        successfully color split, despeckled, masked with the 'Intermodes'
+        threshold, and watershed along with the scale initially set for the
+        necessary ice crystal grey-fill selection step
+                
         Requires path to macro file being modified
     """
     with open(macro_file, "r") as file:
@@ -56,6 +70,41 @@ def modify_macro(
     
     modified_macro = re.sub(open_pattern, open_replace, macro_content)
     
+    if selectIm_configs is None:
+        selectIm_configs = [
+                {'suffix': f'_{time_stamp_str}min_setscale-1.tif (red)"'}, # red generated 1st
+                {'suffix': f'_{time_stamp_str}min_setscale-1.tif (green)"'}, # green generated 2nd
+                {'suffix': f'_{time_stamp_str}min_setscale-1.tif (blue)"'}, # blue generated 3rd
+            ]
+
+    selectIm_counter = 0
+    selectIm_pattern = r'(selectImage\(\"T)(\d+)([^"]*?)(\);)'
+    
+    def selectIm_replace(match):
+        nonlocal select_counter
+        select_counter += 1
+
+        config_idx = min(select_counter - 1, len(selectIm_configs) - 1)
+        config = selectIm_configs[config_idx]
+
+        select_command = match.group(1)
+        T_number = match.group(2)
+        filename_color_strclose = match.group(3)
+        line_close = match.group(4)
+
+        original_line = match.group(0)
+        new_number = file_num_str
+        color_suffix_instance = config['suffix']
+        print(f"\nProcessing selectImage() instance #{save_counter}, line: {original_line[:80]}...")
+        print(f"    Found number: {T_number}")
+        print(f"    Replacing with: {new_number}")
+        print(f"    Original suffixes: '{filename_color_strclose}'")
+        print(f"    Replacing with: '{color_suffix_instance}'")
+
+        return f'{select_command}{new_number}{color_suffix_instance}{line_close}'
+
+    modified_macro = re.sub(selectIm_pattern, selectIm_replace, modified_macro)
+
     if save_configs is None:
         save_configs = [
                 {'suffix': f'_{time_stamp_str}min_setscale'},           # 1st save
@@ -96,6 +145,7 @@ def modify_macro(
     print(f"Modified macro summary:")
     print(f"    File number used: {file_num_str}")
     print(f"    Time stamp used: {time_stamp_str}min")
+    print(f"    Number of selectImage instances modified: {selectIm_counter}")
     print(f"    Number of saveAs instances modified: {save_counter}")
     print(f"\n{'='*50}\n")
     
@@ -116,23 +166,66 @@ def modify_macro(
     print(f"Modified {save_counter} saveAs instances")
     return modified_macro
 
-# I now want to loop this for filenumbers 1, 4, 8, 12, 
+# I now want to loop this for filenumbers 1, 4, 8, 12, 20, 24 and their corresponding times
 filenumbers = [1, 4, 8, 12, 16, 20, 24]
 timestamps = [5, 20, 40, 60, 80, 100, 120]
-target_pairs = list(zip(filenumbers, timestamps))
+trials = [1, 2, 3]
 
+# If you just want to generate one macro file
+# New_macro = modify_macro(
+#    filenumber = 1,
+#    time_stamp = 5,
+#    trial_number = 1,
+#    )
 
+# ij = imagej.init(imagej_path, headless=False)
 
-New_macro = modify_macro(
-    filenumber = 1,
-    time_stamp = 5,
-    trial_number = 1,
+# Full modified_macro_path - this needs to match the pattern of output_path in the function above?
+
+new_macro_path = r'C:\Users\WarrenKincaid\git\notebook_wk_hanlab\ImageJProcessing\Tre_PVA_Images\03_13_24_trial1\Modsplat_autothreshold_updated_um_5min.ijm'
+
+def create_expt_dataframe(
+    file_numbers,
+    time_stamps,
+    trial_numbers
+):
+    """
+    Create a DataFrame for better tracking for each set of desired filenumbers,
+    timestamps, and trials for better experiment tracking
+    """
+    fnum_list = file_numbers
+    ts_list = time_stamps
+    trial_list = trial_numbers
+    data = []
+
+    for trial in trial_list:
+        for fnum, ts in zip(fnum_list, ts_list):
+            data.append({
+                'trial_number': trial,
+                'filenumber': fnum,
+                'time_stamp': ts,
+                'run_id': f"T{trial}_File{fnum}_{ts}min"
+            })
+
+    df = pd.DataFrame(data)
+    
+    return df
+
+df = create_expt_dataframe(
+    file_numbers = filenumbers,
+    time_stamps = timestamps,
+    trial_numbers = trials,
     )
 
-ij = imagej.init(imagej_path, headless=False)
+print(f"\n{'='*50}\n")
+print(f"DataFrame looks like:")
+print(df.head(10))
+print(f"\n{'='*50}\n")
 
-# Full modified_macro_path
-new_macro_path = r'C:\Users\WarrenKincaid\git\notebook_wk_hanlab\ImageJProcessing\Tre_PVA_Images\03_13_24_trial1\Modsplat_autothreshold_updated_5min.ijm'
-
-
+for _, row in df.iterrows():
+    modify_macro(
+        filenumber = row['filenumber'],
+        time_stamp = row['time_stamp'],
+        trial_number = row['trial_number'],
+    )
 
